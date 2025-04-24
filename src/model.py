b64a5aa4e042f85aca23239385474e248ba9210c
Kai Siren <coilysiren@gmail.com>
2025-04-24 16:22:56 -0700

    refining the prompt

--- src/model.py
+++ src/model.py
@@ -1,17 +1,44 @@
 import os
 import requests
+import re
 
 
-def request(prompt: str, n_predict: int = 64) -> str:
+def request(prompt: str, n_predict: int = 128) -> str:
     IS_PRODUCTION = os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/token")
     response = requests.post(
         (
-            "http://llama.llama.cluster.local:8080/completion"
+            "http://llama.llama.cluster.local:8080/v1/chat/completions"
             if IS_PRODUCTION
-            else "http://llama-llama-service:8080/completion"
+            else "http://llama-llama-service:8080/v1/chat/completions"
         ),
-        json={"prompt": prompt, "n_predict": n_predict},
-        timeout=30,
+        json={
+            "messages": [
+                {
+                    "role": "system",
+                    "content": """
+                      Your are a helpful assistant that answers questions about the environment.
+                      Your should give humanized short responses.
+                      Your should not pretend to be a human or represent yourself with words like "I".
+                      Your responses should be complete sentences.
+                    """,
+                },
+                {"role": "user", "content": prompt},
+            ],
+            "max_tokens": n_predict,
+            "temperature": 0.7,
+        },
+        timeout=60,
     )
+    content: str = response.json()["choices"][0]["message"]["content"]
 
-    return response.json()["content"]
+    # Remove leading "\n" and trailing "\n"
+    content = content.strip("\n")
+
+    # Remove sentences that are incomplete,
+    # via removing everything after a period that doesn't end a sentence
+    content = re.sub(r"(?<!\.)\s+(?!\w)", "", content)
+
+    # Remove leading and trailing quotes
+    content = content.strip("\"'")
+
+    return content
